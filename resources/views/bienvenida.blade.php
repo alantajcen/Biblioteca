4bad60876d9ea946033b626364a0af71dd9629d2
Iván Hernández López <ivan.hernandez@biaani.com>
2022-12-23 20:55:08 -0600

    LLamado correcto de JS y CSS

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -8,30 +8,30 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/jqvmap/jqvmap.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('dist/css/adminlte.min.css') }}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/daterangepicker/daterangepicker.css') }}">
 
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/select2/css/select2.css">
-
-
-
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{ URL::asset('plugins/select2/css/select2.css') }}">
 
 
-  <link rel="stylesheet" href="http://localhost/biblioteca-bjg/public/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+
+  <link rel="stylesheet" href="{{ URL::asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+
+
+  <link rel="stylesheet" href="{{ URL::asset('plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
 
 
 
@@ -42,7 +42,7 @@
 
 
   <!-- summernote-->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="{{ URL::Asset('plugins/summernote/summernote-bs4.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
  
@@ -84,58 +84,58 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/jquery/jquery.min.js"></script>
+<script src="{{ URL::asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ URL::asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/chart.js/Chart.min.js"></script>
+<script src="{{ URL::asset('plugins/chart.js/Chart.min.js') }}"></script>
 <!-- Sparkline -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/sparklines/sparkline.js"></script>
+<script src="{{ URL::asset('plugins/sparklines/sparkline.js') }}"></script>
 <!-- JQVMap -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="http://localhost/biblioteca-bjg/public/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ URL::asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 <!-- jQuery Knob Chart -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ URL::asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
 <!-- daterangepicker -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/moment/moment.min.js"></script>
-<script src="http://localhost/biblioteca-bjg/public/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ URL::asset('plugins/moment/moment.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ URL::asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ URL::asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="http://localhost/biblioteca-bjg/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ URL::asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="http://localhost/biblioteca-bjg/public/dist/js/adminlte.js"></script>
+<script src="{{ URL::asset('dist/js/adminlte.js') }}"></script>
 
 <!-- AdminLTE for demo purposes >
-<script src="dist/js/demo.js"></script-->
+<script src="dist/js/demo.js') }}"></script-->
 
 
 
 
-<script src="http://localhost/biblioteca-bjg/public/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
 
-<script src="http://localhost/biblioteca-bjg/public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ URL::asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
-<script src="http://localhost/biblioteca-bjg/public/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-
-
-
-<script src="http://localhost/biblioteca-bjg/public/plugins/inputmask/jquery.inputmask.js"></script>
-
-<script src="http://localhost/biblioteca-bjg/public/plugins/inputmask/inputmask.js"></script>
-
-<script src="http://localhost/biblioteca-bjg/public/plugins/inputmask/inputmask.js"></script>
+<script src="{{ URL::asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
 
 
-<script src="http://localhost/biblioteca-bjg/public/plugins/select2/js/select2.js"></script>
+
+<script src="{{ URL::asset('plugins/inputmask/jquery.inputmask.js') }}"></script>
+
+<script src="{{ URL::asset('plugins/inputmask/inputmask.js') }}"></script>
+
+<script src="{{ URL::asset('plugins/inputmask/inputmask.js') }}"></script>
+
+
+<script src="{{ URL::asset('plugins/select2/js/select2.js') }}"></script>
 
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -382,6 +382,7 @@ $('[data-mask]').inputmask();
 <?php
 
 $exp = explode('/', $_SERVER["REQUEST_URI"]);
+$exp[3] = null;
 
 ?>
 
